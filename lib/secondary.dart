39cfdb91a2bdfbfe10122281a33da8f566b37c20
David Ruiz <david.ruiz@letgo.com>
2024-05-24 13:38:42 +0200

    Forwards and backwards navigation

--- lib/secondary.dart
+++ lib/secondary.dart
@@ -9,32 +9,24 @@ class SecondaryView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Second View',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MySecondView(title: 'Second View'),
-    );
-  }
-}
-
-class MySecondView extends StatefulWidget {
-  const MySecondView({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MySecondView> createState() => _MySecondView();
-}
-
-class _MySecondView extends State<MySecondView> {
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
             backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-            title: Text(widget.title)));
+            title: const Text("BACK")),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton(
+                child: const Text("BACK"),
+                onPressed: () => goBack(context),
+              )
+            ],
+          ),
+        ));
   }
+}
+
+void goBack(BuildContext context) {
+  Navigator.pop(context);
 }
