e3162f5ac1dfe02643a3eb67ce6cd2616a51753f
David Ruiz <david.ruiz@letgo.com>
2024-05-24 13:37:52 +0200

    Second view improvements

--- lib/secondary.dart
+++ lib/secondary.dart
@@ -9,6 +9,32 @@ class SecondaryView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Secondary View');
+    return MaterialApp(
+      title: 'Second View',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MySecondView(title: 'Second View'),
+    );
+  }
+}
+
+class MySecondView extends StatefulWidget {
+  const MySecondView({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MySecondView> createState() => _MySecondView();
+}
+
+class _MySecondView extends State<MySecondView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+            title: Text(widget.title)));
   }
 }
