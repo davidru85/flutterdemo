5242c3452bf7c7c08a94fd4c77c20aaecef969bd
David Ruiz <davidru85@gmail.com>
2024-06-13 12:57:46 +0200

    Adds loop for mock items

--- lib/data/sources/example_data_source.dart
+++ lib/data/sources/example_data_source.dart
@@ -8,11 +8,13 @@ class ExampleDataSourceImpl implements ExampleDataSource {
   @override
   Future<List<ExampleModel>> fetchExamples() async {
     // Mock data source implementation
-    await Future.delayed(Duration(seconds: 1)); // Simulate network delay
-    return [
-      ExampleModel(id: '1', name: 'Example 1'),
-      ExampleModel(id: '2', name: 'Example 2'),
-      ExampleModel(id: '3', name: 'Example 3'),
-    ];
+    await Future.delayed(const Duration(seconds: 5)); // Simulate network delay
+    List<ExampleModel> itemsList = [];
+    for (int i = 0; i < 20; i++) {
+      itemsList.add(
+        ExampleModel(id: "${i+1}", name: 'Example ${i+1}'),
+      );
+    }
+    return itemsList;
   }
 }
