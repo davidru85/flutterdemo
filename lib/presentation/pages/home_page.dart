285edb8384483dc0c0962b49b20306b1d0681509
David Ruiz <davidru85@gmail.com>
2024-06-13 12:39:22 +0200

    Fix stateless stateful error

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -2,20 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../bloc_example_bloc.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late Future<void> _fetchExamplesFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchExamplesFuture = Provider.of<BlocExampleBloc>(context, listen: false).fetchExamples();
+  }
+
   @override
   Widget build(BuildContext context) {
     final bloc = Provider.of<BlocExampleBloc>(context);
 
     return Scaffold(
       appBar: AppBar(title: Text('Home Page')),
-      body: Center(
-        child: FutureBuilder(
-          future: bloc.fetchExamples(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return CircularProgressIndicator();
-            }
+      body: FutureBuilder(
+        future: _fetchExamplesFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return CircularProgressIndicator();
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else {
             return ListView.builder(
               itemCount: bloc.examples.length,
               itemBuilder: (context, index) {
@@ -24,8 +38,8 @@ class HomePage extends StatelessWidget {
                 );
               },
             );
-          },
-        ),
+          }
+        },
       ),
     );
   }
