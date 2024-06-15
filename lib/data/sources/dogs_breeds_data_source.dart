78d4aa4650bfa91760a15c1301a1276e58055c25
David Ruiz <davidru85@gmail.com>
2024-06-15 20:54:38 +0200

    Changes local mock for an actual API request

--- lib/data/sources/dogs_breeds_data_source.dart
+++ lib/data/sources/dogs_breeds_data_source.dart
@@ -1,10 +1,14 @@
+import 'dart:convert';
+
+import 'package:myapp/data/sources/dogs_breeds_api.dart';
+
 import '../models/dogs_breeds_model.dart';
 
 abstract class DogsBreedsDataSource {
   Future<DogsBreedsModel> fetchBreeds();
 }
 
-class DogsBreedsDataSourceImpl implements DogsBreedsDataSource {
+class DogsBreedsDataSourceLocalImpl implements DogsBreedsDataSource {
   @override
   Future<DogsBreedsModel> fetchBreeds() async {
     // Mock data source implementation
@@ -21,7 +25,8 @@ class DogsBreedsDataSourceImpl implements DogsBreedsDataSource {
               femaleWeight: WeightModel(max: 10, min: 1),
               maleWeight: WeightModel(max: 10, min: 1)),
           relationships: RelationshipsModel(
-              group: GroupModel(data: RelationshipDataModel(id: "rel1", type: "type1")))),
+              group: GroupModel(
+                  data: RelationshipDataModel(id: "rel1", type: "type1")))),
       BreedsDataModel(
           id: "2",
           type: "DOS",
@@ -32,13 +37,38 @@ class DogsBreedsDataSourceImpl implements DogsBreedsDataSource {
               femaleWeight: WeightModel(max: 20, min: 2),
               maleWeight: WeightModel(max: 20, min: 2)),
           relationships: RelationshipsModel(
-              group: GroupModel(data: RelationshipDataModel(id: "rel2", type: "type2"))))
+              group: GroupModel(
+                  data: RelationshipDataModel(id: "rel2", type: "type2"))))
     ];
 
-    breeds.links =
-        LinksModel(self: "self", current: "current", next: "next", last: "last");
+    breeds.links = LinksModel(
+        self: "self", current: "current", next: "next", last: "last");
     breeds.data = itemsList;
 
     return breeds;
+  }
+}
+
+class DogsBreedsDataSourceRemoteImpl implements DogsBreedsDataSource {
+  DogsBreedsApi apiService;
+
+  DogsBreedsDataSourceRemoteImpl(this.apiService);
+
+  @override
+  Future<DogsBreedsModel> fetchBreeds() async {
+  
+    try {
+      final response = await apiService.getRequest("breeds");
+      DogsBreedsModel model = DogsBreedsModel.fromJson(jsonDecode(response.body));
+      return model;
+      //setState(() {
+        //getData = response.body;
+      //});
+    } catch (e) {
+      return DogsBreedsModel();
+      //setState(() {
+        //getData = 'Error: $e';
+      //});
+    }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/data/repositories/dogs_breeds_repository_impl.dart';
+import 'package:myapp/data/sources/dogs_breeds_api.dart';
 import 'package:myapp/data/sources/dogs_breeds_data_source.dart';
 import 'package:myapp/domain/usecases/get_dogs_breeds_usecase.dart';
 import 'package:myapp/presentation/dogs_breeds_presenter.dart';
@@ -24,7 +25,7 @@ void main() {
         ChangeNotifierProvider(
           create: (_) => DogsBreedsPresenter(
             getDogsBreedsUseCase: GetDogsBreedsUseCase(
-              DogsBreedsRepositoryImpl(DogsBreedsDataSourceImpl()),
+              DogsBreedsRepositoryImpl(DogsBreedsDataSourceRemoteImpl(DogsBreedsApi("https://dogapi.dog/api/v2"))),
             ),
           ),
         ),
